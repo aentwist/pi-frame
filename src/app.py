97f12f4d8a273e85339c49b61b1023e35b28bae8
Anderson Entwistle <acentwistle01@gmail.com>
2020-07-10 14:01:52 -0400

    add snippet to display contents

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
+import os
+
 from flask import Flask
 
 
+UPLOAD_FOLDER = "/var/www/full-frame/uploads"
+ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png"}
+
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-@app.route("/")
-def index():
-    return "hello, world"
+@app.route("/<path>")
+def index(path):
+    _, dirs, files = next(os.walk(f"./{path}"))
+    return render_template("index", path, dirs, files)
